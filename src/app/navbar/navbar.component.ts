7d923ac028e582a29982f5a6e0c5da921eccbd55
Tsuyoshi HARA <chloe463+git@gmail.com>
2017-01-25 23:59:42 +0900

    feat(navbar): Add fr-navbar and related components

---
+++ src/app/navbar/navbar.component.ts
@@ -0,0 +1,37 @@
+import {
+    Component,
+    OnInit,
+    Input,
+    ContentChildren,
+    QueryList
+} from '@angular/core';
+import {
+    Location,
+    LocationStrategy,
+    PathLocationStrategy,
+    HashLocationStrategy
+} from '@angular/common';
+import { NavbarLogoComponent } from './navbar-logo.component';
+import { NavbarItemComponent } from './navbar-item.component';
+
+@Component({
+  selector: 'fr-navbar',
+  templateUrl: './navbar.component.html',
+  styleUrls: ['./navbar.component.scss'],
+  providers: [Location, { provide: LocationStrategy, useClass: HashLocationStrategy }]
+})
+export class NavbarComponent implements OnInit {
+
+    @ContentChildren(NavbarLogoComponent) _logos: QueryList<NavbarLogoComponent>;
+    @ContentChildren(NavbarItemComponent) _items: QueryList<NavbarItemComponent>;
+
+    constructor(private _location: Location) {
+    }
+
+    ngOnInit() {
+    }
+
+    private _isSelected(link: string) {
+        return link.substr(1) === this._location.path();
+    }
+}
