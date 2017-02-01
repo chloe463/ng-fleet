b7fb7ab09257e84aa07741fc5044fecb423ef547
Tsuyoshi HARA <chloe463+git@gmail.com>
2017-02-02 01:12:30 +0900

    build(scss): Add task to compile scss to css

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,26 @@
 var path = require('path');
 var gulp = require('gulp');
 var gulpBetterRollup = require('gulp-better-rollup');
+var gulpSass = require('gulp-sass');
+var autoPrefixer = require('gulp-autoprefixer');
+var gulpRename = require('gulp-rename');
 
 const PROJECT_ROOT = path.join(__dirname);
-const SOURCE_ROOT  = path.join(__dirname, 'src/app');
+const SOURCE_ROOT  = path.join(__dirname, 'src');
 const DIST_ROOT    = path.join(__dirname, 'dist/out-tsc');
 const BUNDLES_ROOT = path.join(__dirname, 'bundles');
 
+// scss -> css
+gulp.task('build:css', () => {
+    const TARGET_FILE = path.join(SOURCE_ROOT, 'styles.scss');
+    return gulp.src(TARGET_FILE)
+        .pipe(gulpSass())
+        .pipe(autoPrefixer())
+        .pipe(gulpRename('francette.css'))
+        .pipe(gulp.dest(BUNDLES_ROOT));
+});
+
+// bundle js files
 gulp.task('rollup', () => {
     const TARGET_FILE  = path.join(DIST_ROOT, 'app/index.js');
 
