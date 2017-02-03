db87fb8c31455d08f232d3c97cb7b9b2ee176a20
Tsuyoshi HARA <chloe463+git@gmail.com>
2017-02-04 00:50:34 +0900

    feat(gulp-task): Make it possible to replace inline resources(templates and styles

--- gulpfile.js
+++ gulpfile.js
@@ -6,11 +6,72 @@ const autoPrefixer     = require('gulp-autoprefixer');
 const gulpRename       = require('gulp-rename');
 const gulpCleanCss     = require('gulp-clean-css');
 const gulpUglify       = require('gulp-uglify');
+const glob             = require('glob');
+const fs               = require('fs');
+const del              = require('del');
 
 const PROJECT_ROOT = path.join(__dirname);
 const SOURCE_ROOT  = path.join(__dirname, 'src');
 const DIST_ROOT    = path.join(__dirname, 'dist/out-tsc');
 const BUNDLES_ROOT = path.join(__dirname, 'bundles');
+
+function promiseify(fn) {
+    return function() {
+        const args = [].slice.call(arguments, 0);
+        return new Promise((resolve, reject) => {
+            fn.apply(this, args.concat([function (err, value) {
+                if (err) {
+                    reject(err);
+                } else {
+                    resolve(value);
+                }
+            }]));
+        });
+    };
+}
+
+const readFile = promiseify(fs.readFile);
+
+function replaceTemplate(content, filePath) {
+    const templatePath    = filePath.replace(/dist\/out-tsc/, 'src/app').replace(/\.js$/, '.html');
+    if (!fs.existsSync(templatePath)) {
+        return content.replace(/templateUrl: \'.*html\'/, 'template: ``');
+    }
+    const templateContent = fs.readFileSync(templatePath, 'utf-8');
+    return content.replace(/templateUrl: \'.*html\'/, 'template: `' + templateContent + '`')
+}
+
+function replaceStyle(content, filePath) {
+    return content.replace(/styleUrls:\s*(\[[\s\S]*?\])/, function (m, styleUrls) {
+        const urls = eval(styleUrls);
+
+        const styles = urls.map((url) => {
+            const dir       = path.dirname(filePath).replace(/dist\/out-tsc/, 'src/app');
+            const stylePath = path.join(dir, url);
+            const styleContent = fs.readFileSync(stylePath, 'utf-8');
+            const shortened    = styleContent
+                .replace(/([\n\r]\s*)+/gm, ' ')
+                .replace(/"/gm, '\\"');
+            return `"${shortened}"`;
+        });
+
+        return 'styles: [' + styles.join(',\n') + ']';
+    });
+}
+
+gulp.task('bundle:inline-resource', () => {
+    const files = glob(DIST_ROOT + '/**/**.js', (err, files) => {
+        files.map((filePath) => {
+            readFile(filePath, 'utf-8').then((content) => {
+                content = replaceTemplate(content, filePath);
+                content = replaceStyle(content, filePath);
+                fs.writeFileSync(filePath, content);
+            }).catch((err) => {
+                console.error(err);
+            });
+        });
+    });
+});
 
 // scss -> css
 gulp.task('build:css', () => {
@@ -33,7 +94,7 @@ gulp.task('minify:css', () => {
 
 // bundle js files
 gulp.task('rollup', () => {
-    const TARGET_FILE  = path.join(DIST_ROOT, 'app/index.js');
+    const TARGET_FILE  = path.join(DIST_ROOT, 'index.js');
 
     const globals = {
         '@angular/core': 'ng.core',
