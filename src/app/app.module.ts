f859052e7b4443844201b0c5f9ffbecf0f143dc5
Tsuyoshi HARA <chloe463+git@gmail.com>
2017-02-16 01:41:38 +0900

    improve(navbar): Use @angular/router directives

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,19 +2,35 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import {
+  FrNavSample1Component,
+  FrNavSample2Component,
+  FrNavSample3Component,
+} from './app.component';
 
 import { FrancetteModule } from './francette.module';
+
+const routeConfig: Routes = [
+  { path: 'nav1', component: FrNavSample1Component },
+  { path: 'nav2', component: FrNavSample2Component },
+  { path: 'nav3', component: FrNavSample3Component },
+];
 
 @NgModule({
   declarations: [
     AppComponent,
+    FrNavSample1Component,
+    FrNavSample2Component,
+    FrNavSample3Component
   ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpModule,
+    RouterModule.forRoot(routeConfig),
     FrancetteModule
   ],
   exports: [],
