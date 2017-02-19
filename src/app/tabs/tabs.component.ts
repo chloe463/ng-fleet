578d147869f5c163c0e310f1eb0756fa074ac9b9
Tsuyoshi HARA <chloe463+git@gmail.com>
2017-02-19 14:31:05 +0900

    improve(tabs): Improve tab selection process (2 loops -> 1 loop)

--- src/app/tabs/tabs.component.ts
+++ src/app/tabs/tabs.component.ts
@@ -38,19 +38,16 @@ export class FrTabsComponent implements OnInit, AfterContentInit, OnChanges {
   }
 
   public select(tab: FrTabComponent): void {
+    let state = 'right';
     this._tabs.forEach((_tab: FrTabComponent, _index: number) => {
+      _tab.state    = state;
       _tab.selected = false;
       if (tab === _tab) {
         _tab.selected = true;
         _tab.state    = 'center';
         this._current = {index: _index, tab: _tab};
-      }
-    });
-    this._tabs.forEach((_tab: FrTabComponent, _index: number) => {
-      if (this._current.index < _index) {
-        _tab.state = 'right';
-      } else if (this._current.index > _index) {
-        _tab.state = 'left';
+        state         = 'left';
+        this.selectedIndex = _index;
       }
     });
   }
