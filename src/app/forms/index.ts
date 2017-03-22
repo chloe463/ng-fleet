519bc41c9eb251bba563f040747b4c932df362f4
Tsuyoshi HARA <chloe463+git@gmail.com>
2017-03-22 23:50:14 +0900

    refactor: Fix export statements
    Add missing export statements
    
    Remove unused codes

--- src/app/forms/index.ts
+++ src/app/forms/index.ts
@@ -1,7 +1,6 @@
-export { FrInputTextComponent } from './input-text/input-text.component';
-export { FrDatePickerComponent } from './date-picker/date-picker.component';
 export { FrCheckboxComponent } from './checkbox/checkbox.component';
-export { FrRadioGroupComponent } from './radio/radio-group.component';
+export { FrDatePickerComponent } from './date-picker/date-picker.component';
+export { FrInputTextComponent } from './input-text/input-text.component';
 export { FrRadioComponent } from './radio/radio.component';
 export { FrOptionComponent } from './select/option.component';
 export { FrSelectComponent } from './select/select.component';
--- src/app/navbar/index.ts
+++ src/app/navbar/index.ts
@@ -1,3 +1,4 @@
-export { FrNavbarComponent } from './navbar.component';
 export { FrNavbarLogoComponent } from './navbar-logo.component';
 export { FrNavbarItemComponent } from './navbar-item.component';
+export { FrNavbarMenuComponent } from './navbar-menu.component';
+export { FrNavbarComponent } from './navbar.component';
