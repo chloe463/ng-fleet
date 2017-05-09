6c916bef9ebc38f2a5a09a1aba1fbf88d6a388be
Tsuyoshi HARA <chloe463+git@gmail.com>
2017-05-10 00:06:17 +0900

    refactor(demo): Add form-demo component

--- src/demo/app.component.ts
+++ src/demo/app.component.ts
@@ -7,11 +7,6 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'Francette';
   selectedIndex = 0;
-  chks = [
-    { name: 'chk1', label: 'chk1' },
-    { name: 'chk2', label: 'chk2' },
-    { name: 'chk3', label: 'chk3' },
-  ];
   data = {
     dateList: [
       20170101, 20170102, 20170103, 20170104, 20170105, 20170106, 20170107
@@ -22,56 +17,6 @@ export class AppComponent {
       { name: 'row2', values: [3, 4, 5, 6, 7, 8, 9] },
     ]
   };
-  options = [
-    {value: 0, label: 'value1'},
-    {value: 1, label: 'value2'},
-    {value: 2, label: 'value3'},
-  ];
-  selectValues = [
-    {key: 1, value: 'val1'},
-    {key: 2, value: 'val2'},
-  ];
-  selectDisabled = true;
-  form: {[key: string]: any} = {
-    text: '',
-    select: '',
-    radio: '',
-    checkbox: {},
-    date: new Date(),
-    time: new Date()
-  };
-
-  public toggleSelect() {
-    this.selectDisabled = !this.selectDisabled;
-  }
-
-  public submit() {
-    console.log(this.form);
-  }
-
-  public resetSelect() {
-    this.options = [
-      {value: 9, label: 'value9'},
-      {value: 8, label: 'value8'},
-      {value: 7, label: 'value7'},
-    ];
-  }
-
-  public reset() {
-    this.form = {
-      text: '',
-      select: '',
-      radio: '',
-      checkbox: {},
-      date: new Date(),
-      time: new Date()
-    };
-  }
-
-  public onSubmit(e) {
-    console.log(e);
-    return false;
-  }
 
 }
 
--- src/demo/app.module.ts
+++ src/demo/app.module.ts
@@ -7,19 +7,22 @@ import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
 import { DataTableDemoComponent } from './data-table-demo.component';
 import { ChipsDemoComponent } from './chips-demo.component';
+import { FormsDemoComponent } from './forms-demo.component';
 
 import { FrancetteModule } from '../app/francette.module';
 
 const routeConfig: Routes = [
   { path: 'data-table', component: DataTableDemoComponent },
-  { path: 'chips', component: ChipsDemoComponent }
+  { path: 'chips', component: ChipsDemoComponent },
+  { path: 'forms', component: FormsDemoComponent }
 ];
 
 @NgModule({
   declarations: [
     AppComponent,
     DataTableDemoComponent,
-    ChipsDemoComponent
+    ChipsDemoComponent,
+    FormsDemoComponent
   ],
   imports: [
     BrowserModule,
