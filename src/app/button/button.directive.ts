8aa57e21550ca8d3501ba4e117d0a27f49b80411
Tsuyoshi HARA <chloe463+git@gmail.com>
2018-01-16 22:13:54 +0900

    fix(button): Fix deprecated notation

--- src/app/button/button.directive.ts
+++ src/app/button/button.directive.ts
@@ -1,88 +1,101 @@
-import { Directive } from '@angular/core';
+import { Directive, HostBinding } from '@angular/core';
 
 /**
  * Normal buttons
  */
 @Directive({
-  selector: 'button[frButton], button[fr-button]',
-  host: { 'class': 'fr-btn' }
+  selector: 'button[frButton], button[fr-button]'
 })
-export class FrButtonDirective {}
+export class FrButtonDirective {
+  @HostBinding('class') btn = 'fr-btn';
+}
 
 @Directive({
-  selector: 'button[frPrimaryButton], button[fr-primary-button]',
-  host: { 'class': 'fr-btn fr-btn--primary' }
+  selector: 'button[frPrimaryButton], button[fr-primary-button]'
 })
-export class FrPrimaryButtonDirective {}
+export class FrPrimaryButtonDirective {
+  @HostBinding('class') btn = 'fr-btn fr-btn--primary';
+}
 
 @Directive({
-  selector: 'button[frInfoButton], button[fr-info-button]',
-  host: { 'class': 'fr-btn fr-btn--info' }
+  selector: 'button[frInfoButton], button[fr-info-button]'
 })
-export class FrInfoButtonDirective {}
+export class FrInfoButtonDirective {
+  @HostBinding('class') btn = 'fr-btn fr-btn--info';
+}
 
 @Directive({
-  selector: 'button[frWarningButton], button[fr-warning-button]',
-  host: { 'class': 'fr-btn fr-btn--warning' }
+  selector: 'button[frWarningButton], button[fr-warning-button]'
 })
-export class FrWarningButtonDirective {}
+export class FrWarningButtonDirective {
+  @HostBinding('class') btn = 'fr-btn fr-btn--warning';
+}
 
 @Directive({
-  selector: 'button[frDangerButton], button[fr-danger-button]',
-  host: { 'class': 'fr-btn fr-btn--danger' }
+  selector: 'button[frDangerButton], button[fr-danger-button]'
 })
-export class FrDangerButtonDirective {}
+export class FrDangerButtonDirective {
+  @HostBinding('class') btn = 'fr-btn fr-btn--danger';
+}
 
 @Directive({
-  selector: 'button[frDisabledButton], button[fr-disabled-button]',
-  host: { 'class': 'fr-btn fr-btn--disabled' }
+  selector: 'button[frDisabledButton], button[fr-disabled-button]'
 })
-export class FrDisabledButtonDirective {}
+export class FrDisabledButtonDirective {
+  @HostBinding('class') btn = 'fr-btn fr-btn--disabled';
+}
 
 /**
  * Skeleton buttons
  */
 @Directive({
-  selector: 'button[frSkeletonButton], button[fr-skeleton-button]',
-  host: { 'class': 'fr-btn-skeleton' }
+  selector: 'button[frSkeletonButton], button[fr-skeleton-button]'
 })
-export class FrSkeletonButtonDirective {}
+export class FrSkeletonButtonDirective {
+  @HostBinding('class') btn = 'fr-btn fr-btn-skeleton';
+}
 
 @Directive({
-  selector: 'button[frSkeletonPrimaryButton], button[fr-skeleton-primary-button]',
-  host: { 'class': 'fr-btn-skeleton--primary' }
+  selector: 'button[frSkeletonPrimaryButton], button[fr-skeleton-primary-button]'
 })
-export class FrSkeletonPrimaryButtonDirective {}
+export class FrSkeletonPrimaryButtonDirective {
+  @HostBinding('class') btn = 'fr-btn fr-btn-skeleton--primary';
+}
 
 @Directive({
-  selector: 'button[frSkeletonInfoButton], button[fr-skeleton-info-button]',
-  host: { 'class': 'fr-btn-skeleton--info' }
+  selector: 'button[frSkeletonInfoButton], button[fr-skeleton-info-button]'
 })
-export class FrSkeletonInfoButtonDirective {}
+export class FrSkeletonInfoButtonDirective {
+  @HostBinding('class') btn = 'fr-btn fr-btn-skeleton--info';
+}
 
 @Directive({
-  selector: 'button[frSkeletonWarningButton], button[fr-skeleton-warning-button]',
-  host: { 'class': 'fr-btn-skeleton--warning' }
+  selector: 'button[frSkeletonWarningButton], button[fr-skeleton-warning-button]'
 })
-export class FrSkeletonWarningButtonDirective {}
+export class FrSkeletonWarningButtonDirective {
+  @HostBinding('class') btn = 'fr-btn fr-btn-skeleton--warning';
+}
 
 @Directive({
-  selector: 'button[frSkeletonDangerButton], button[fr-skeleton-danger-button]',
-  host: { 'class': 'fr-btn-skeleton--danger' }
+  selector: 'button[frSkeletonDangerButton], button[fr-skeleton-danger-button]'
 })
-export class FrSkeletonDangerButtonDirective {}
+export class FrSkeletonDangerButtonDirective {
+  @HostBinding('class') btn = 'fr-btn fr-btn-skeleton--danger';
+}
 
 @Directive({
-  selector: 'button[frSkeletonDisabledButton], button[fr-skeleton-disabled-button]',
-  host: { 'class': 'fr-btn-skeleton--disabled' }
+  selector: 'button[frSkeletonDisabledButton], button[fr-skeleton-disabled-button]'
 })
-export class FrSkeletonDisabledButtonDirective {}
+export class FrSkeletonDisabledButtonDirective {
+  @HostBinding('class') btn = 'fr-btn fr-btn-skeleton--disabled';
+}
 
 /**
  * Flat button
  */
 @Directive({
-  selector: 'button[frFlatButton], button[fr-flat-button]',
-  host: { 'class': 'fr-btn-flat' }
+  selector: 'button[frFlatButton], button[fr-flat-button]'
 })
-export class FrFlatButtonDirective {}
+export class FrFlatButtonDirective {
+  @HostBinding('class') btn = 'fr-btn fr-btn-flat';
+}
