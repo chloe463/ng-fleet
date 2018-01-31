16312e9bdcd8955be814f50fd7e92203d9f39061
Tsuyoshi HARA <chloe463+git@gmail.com>
2018-01-31 21:03:29 +0900

    hotfix(ripple): Fix ripple position

--- src/app/ripple/ripple.directive.ts
+++ src/app/ripple/ripple.directive.ts
@@ -19,32 +19,24 @@ export class FrRippleDirective {
   constructor(private _el: ElementRef) {
   }
 
-  @HostListener('click', ['$event'])
-  @HostListener('drop', ['$event'])
+  @HostListener('mousedown', ['$event'])
   public onClick(event: MouseEvent) {
     const element = this._el.nativeElement;
 
-    let offsetLeft = element.offsetLeft;
-    let offsetTop  = element.offsetTop;
-    if (element.offsetParent.offsetTop) {
-      offsetTop += element.offsetParent.offsetTop;
-    }
-    if (element.offsetParent.offsetLeft) {
-      offsetLeft += element.offsetParent.offsetLeft;
-    }
-
-    const xPos = event.pageX - offsetLeft;
-    const yPos = event.pageY - offsetTop;
-
     const rippleHeight = Math.max(element.clientWidth, element.clientHeight);
     const rippleWidth  = Math.max(element.clientWidth, element.clientHeight);
+
+    const location = element.getBoundingClientRect();
+
+    const top  = event.pageY - location.top  - window.pageYOffset - (rippleHeight / 2);
+    const left = event.pageX - location.left - window.pageXOffset - (rippleWidth / 2);
 
     const ripple = document.createElement('div');
     ripple.style.setProperty('background', this.rippleColor || 'white');
     ripple.style.setProperty('height', `${rippleHeight}px`);
     ripple.style.setProperty('width',  `${rippleWidth}px`);
-    ripple.style.setProperty('top',    `${yPos - rippleHeight / 2}px`);
-    ripple.style.setProperty('left',   `${xPos - rippleWidth / 2}px`);
+    ripple.style.setProperty('top',    `${top}px`);
+    ripple.style.setProperty('left',   `${left}px`);
     ripple.classList.add('fr-ripple-effect');
     element.appendChild(ripple);
 
