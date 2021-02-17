df372540a9413d08ea715413217be4f4a6d2089f
Tsuyoshi HARA <chloe463+git@gmail.com>
2021-02-18 01:04:52 +0900

    build: Uninstall @angular/http

--- src/demo/app.module.ts
+++ src/demo/app.module.ts
@@ -1,7 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
 import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
@@ -58,7 +57,6 @@ const routeConfig: Routes = [
     BrowserModule,
     FormsModule,
     ReactiveFormsModule,
-    HttpModule,
     RouterModule.forRoot(routeConfig, { relativeLinkResolution: 'legacy' }),
     FrancetteModule
   ],
