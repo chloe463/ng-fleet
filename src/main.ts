057c9fef7934c1f1a65695b98247cab64f7ca1b9
Tsuyoshi HARA <chloe463+git@gmail.com>
2021-02-28 01:16:13 +0900

    change(demo): Move demo to app

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,8 @@
-import './polyfills.ts';
-
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { enableProdMode } from '@angular/core';
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
-import { AppModule } from './demo/app.module';
+import './polyfills.ts';
 
 if (environment.production) {
   enableProdMode();
