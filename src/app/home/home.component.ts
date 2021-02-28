135658c4dcd8f9df7b08d785902abd4d60dcbde3
Tsuyoshi HARA <chloe463+git@gmail.com>
2021-03-01 01:34:13 +0900

    feat(demo/home): Add components cards

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -6,6 +6,53 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
+  public components = [
+    {
+      name: "Buttons",
+      image: "assets/images/buttons.png",
+      url: "/buttons",
+    },
+    {
+      name: "Chips",
+      image: "assets/images/chips.png",
+      url: "/chips",
+    },
+    {
+      name: "Data-table",
+      image: "assets/images/data-table.png",
+      url: "/data-table",
+    },
+    {
+      name: "Dialog",
+      image: "assets/images/dialog.png",
+      url: "/dialog",
+    },
+    {
+      name: "Forms",
+      image: "assets/images/forms.png",
+      url: "/forms",
+    },
+    {
+      name: "Progress",
+      image: "assets/images/progress-spinner.png",
+      url: "/progress",
+    },
+    {
+      name: "Tabs",
+      image: "assets/images/tabs.png",
+      url: "/tabs",
+    },
+    {
+      name: "Toaster",
+      image: "assets/images/toast.png",
+      url: "/toaster",
+    },
+    {
+      name: "Tooltip",
+      image: "assets/images/tooltip.png",
+      url: "/tooltip",
+    }
+  ];
 
   constructor() { }
 
